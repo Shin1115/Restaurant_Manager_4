2f4e8417c71811cd67b3bfd4cc0bfce4d43fda35
truongaxin123 <truongaxin123@gmail.com>
2021-11-19 10:56:13 +0700

    edit configs

--- Restaurant_Manager_4/Global.asax.cs
+++ Restaurant_Manager_4/Global.asax.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using Restaurant_Manager_4.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using Newtonsoft.Json;
 
 namespace Restaurant_Manager_4
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -16,6 +19,25 @@ namespace Restaurant_Manager_4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Request.Cookies["Authentication"];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                var serializeModel = JsonConvert.DeserializeObject<CustomSerializeModel>(authTicket.UserData);
+                CustomPrincipal principal = new CustomPrincipal(authTicket.Name);
+                principal.UserId = serializeModel.UserId;
+                principal.UserName = serializeModel.Username;
+                principal.PhoneNumber = serializeModel.PhoneNumber;
+                principal.Email = serializeModel.Email;
+                principal.Roles = serializeModel.RoleName.ToArray<string>();
+
+                HttpContext.Current.User = principal;
+            }
         }
     }
 }
