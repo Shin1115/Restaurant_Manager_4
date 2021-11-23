7d335e5d8d6dcf7a432caa90d5a188716ecad88d
Quốc Huy <kudoshin991@gmail.com>
2021-11-23 20:42:28 +0700

    add controller admin and css admin

---
+++ Restaurant_Manager_4/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant_Manager_4.Models;
+
+namespace Restaurant_Manager_4.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+    }
+}
