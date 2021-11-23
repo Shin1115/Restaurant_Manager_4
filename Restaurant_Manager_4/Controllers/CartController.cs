24784fa5620f44723957394e1b867f044e0c3e26
Quốc Huy <kudoshin991@gmail.com>
2021-11-23 20:43:05 +0700

    Add controller Cart and MonAn

---
+++ Restaurant_Manager_4/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Restaurant_Manager_4.Models;
+namespace Restaurant_Manager_4.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        QuanLyNhaHangDataContext db = new QuanLyNhaHangDataContext();
+        private string strCart = "Cart";
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
