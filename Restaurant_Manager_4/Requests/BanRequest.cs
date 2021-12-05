aef6383bde9b829795cf21186edc7aaff2df4df2
truongaxin123 <truongaxin123@gmail.com>
2021-12-05 23:21:52 +0700

    add requests

---
+++ Restaurant_Manager_4/Requests/BanRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_Manager_4.Requests
+{
+    public class BanRequest
+    {
+        [DisplayName("Tên bàn")]
+        [Required(ErrorMessage ="Phải có tên")]
+        public string ten_ban { get; set; }
+
+        [Required(ErrorMessage ="Phải có chỗ ngồi tối đa")]
+        [Range(1, 10, ErrorMessage ="Chỗi ngồi tối đa nằm trong khoảng từ 1-10")]
+        [DisplayName("Chỗ ngồi tối đa")]
+        public int cho_ngoi_toi_da { get; set; }
+
+        [Required(ErrorMessage = "Bạn chưa chọn trạng thái bàn")]
+        [DisplayName("Trạng thái bàn")]
+        public int trang_thai { get; set; }
+
+        [Required(ErrorMessage ="Phải có đặt cọc tối thiểu")]
+        [DisplayName("Đặt cọc tối thiểu")]
+        public int dat_coc_toi_thieu { get; set; }
+
+        [DisplayName("Tải ảnh lên")]
+        public string hinh_anh { get; set; }
+
+        [Required(ErrorMessage = "Phải kèm theo một ảnh")]
+        [NotMapped]
+        public HttpPostedFileBase imageFile { get; set; }
+
+        [DisplayName("Mô tả")]
+        public string mo_ta { get; set; }
+    }
+}
---
+++ Restaurant_Manager_4/Requests/DatBanRequest.cs
@@ -0,0 +1,18 @@
+﻿using Restaurant_Manager_4.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_Manager_4.Requests
+{
+    public class DatBanRequest
+    {
+        public DateTime ngay_dat { get; set; }
+        public int tien_coc_truoc { get; set; }
+        public int ngay_den { get; set; }
+        public List<BanDTO> banDTOs { get; set; }
+        public List<MonAnDTO> monAnDTOs { get; set; }
+        public int so_luong_nguoi { get; set; }
+    }
+}
