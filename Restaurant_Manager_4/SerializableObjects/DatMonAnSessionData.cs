5c29ceb7016b2b51311a19ab31cfe482ac872d1e
truongaxin123 <truongaxin123@gmail.com>
2021-12-05 23:22:01 +0700

    add session objects

---
+++ Restaurant_Manager_4/SerializableObjects/DatMonAnSessionData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant_Manager_4.SerializableObjects
+{
+    [Serializable]
+    public class DatMonAnSessionData
+    {
+        public List<int> Bans { get; set; } = new List<int>();
+        public List<int> MonAns { get; set; } = new List<int>();
+    }
+}
