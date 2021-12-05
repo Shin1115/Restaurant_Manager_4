4fd86a6ec8e40d4172fb1f8fae8e650de80cf3a1
truongaxin123 <truongaxin123@gmail.com>
2021-12-05 23:20:57 +0700

    add dto

--- Restaurant_Manager_4/DTO/BanDTO.cs
+++ Restaurant_Manager_4/DTO/BanDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,22 @@ namespace Restaurant_Manager_4.DTO
     public class BanDTO
     {
         public int id { get; set; }
+        [DisplayName("Tên bàn")]
         public string ten_ban { get; set; }
+
+        [DisplayName("Chỗ ngồi tối đa")]
         public int cho_ngoi_toi_da { get; set; }
+
+        [DisplayName("Trạng thái")]
         public string trang_thai { get; set; }
-        public int dat_coc_toi_thieu { get; set; }
+
+        [DisplayName("Đặt cọc tối thiểu")]
+        public string dat_coc_toi_thieu { get; set; }
+
+        [DisplayName("Hình ảnh")]
         public string hinh_anh { get; set; }
+
+        [DisplayName("Mô tả")]
         public string mo_ta { get; set; }
     }
 }
