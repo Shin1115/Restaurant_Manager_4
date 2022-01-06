a28d9640e0fe3a5f4f556c3db2f49fd196c3cf9b
Quốc Huy <kudoshin991@gmail.com>
2022-01-06 11:49:20 +0700

    cart

---
+++ Restaurant_Manager_4/Models/Cart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Restaurant_Manager_4.Models
+{
+    public class Cart
+    {
+        public mon_an Monan { get; set; }
+        public int Soluong { get; set; }
+
+        public Cart(mon_an monan, int soluong)
+        {
+            Monan = monan;
+            Soluong = soluong;
+        }
+    }
+}
