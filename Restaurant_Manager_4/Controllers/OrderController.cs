29b6aa20e9293c22827f22e284cd3ca2b6d4d892
Quốc Huy <kudoshin991@gmail.com>
2022-01-06 11:48:22 +0700

    Order and Cart controller

---
+++ Restaurant_Manager_4/Controllers/OrderController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Restaurant_Manager_4.Models;
+
+namespace Restaurant_Manager_4.Controllers
+{
+    public class OrderController : Controller
+    {
+        QuanLyNhaHangDataContext db = new QuanLyNhaHangDataContext();
+        // GET: Order
+        public ActionResult Index(int? page)
+        {
+            var pageNumber = page ?? 1;
+            var pageSize = 6;
+            List<mon_an> lstdatban = db.mon_an.ToList();
+            var monanlst = db.mon_an.OrderByDescending(x => x.id).ToList().ToPagedList(pageNumber, pageSize);
+            return View(monanlst);
+        }
+
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
