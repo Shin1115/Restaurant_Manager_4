86f255e5bfda4d0a291f832f798aea2ebfa0fd8b
truongaxin123 <truongaxin123@gmail.com>
2022-01-06 12:04:01 +0700

    fix routes

--- Restaurant_Manager_4/App_Start/RouteConfig.cs
+++ Restaurant_Manager_4/App_Start/RouteConfig.cs
@@ -10,42 +10,6 @@ namespace Restaurant_Manager_4
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "ChonBan",
-                url: "DatBan/Index/{idPhanMuc}",
-                defaults: new { controller = "Datban", action = "Index", idPhanMuc = UrlParameter.Optional }
-                );
-
-            routes.MapRoute(
-                name: "XoaPhanMuc",
-                url: "DatBan/XoaPhanMuc/{idPhanMuc}",
-                defaults: new { controller = "Datban", action = "XoaPhanMuc", idPhanMuc = UrlParameter.Optional}
-                );
-
-            routes.MapRoute(
-                name: "DatBanController",
-                url: "DatBan/ThemBan/{idBan}/{idPhanMuc}",
-                defaults: new { controller = "Datban", action = "ThemBan", idBan = UrlParameter.Optional, idPhanMuc = UrlParameter.Optional }
-                );
-
-            routes.MapRoute(
-                name: "XoaBanDaDat",
-                url: "DatBan/XoaBan/{idBan}/{idPhanMuc}",
-                defaults: new { controller = "Datban", action = "XoaBan", idBan = UrlParameter.Optional, idPhanMuc = UrlParameter.Optional }
-                );
-
-            routes.MapRoute(
-                name: "ThemBanVaoPhanMuc",
-                url: "DatBan/Index/{idPhanMuc}",
-                defaults: new { controller = "Datban", action = "Index", idPhanMuc = UrlParameter.Optional }
-                );
-
-            routes.MapRoute(
-                name: "BaoCaoThongKe",
-                url: "BaoCaoThongKe/{action}/{id}",
-                defaults: new { controller = "BaoCao", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
                 name: "DanhMucMonAn",
                 url: "DanhMucMonAn/{action}/{TenMonAn}",
                 defaults: new { controller = "DanhMucMonAn", action = "List", name = UrlParameter.Optional }
